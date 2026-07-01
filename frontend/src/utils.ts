8c9ada0b10e3f260c7a7846f6d59d84a014b39b3
Danil Berezin <berezindanil2004@gmail.com>
2026-07-01 12:53:09 +0800

    add: new screen

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -2,7 +2,7 @@ import { MaterialProps, DeliveryOption } from "./MaterialDetailScreen";
 
 export const baseURL = "https://darmavoz.ru/api/v1";
 
-export const APP_VERSION = import.meta.env.VITE_APP_VERSION || "2.4.0";
+export const APP_VERSION = import.meta.env.VITE_APP_VERSION || "2.4.1";
 
 export const playNewOrderSound = () => {
   try {
